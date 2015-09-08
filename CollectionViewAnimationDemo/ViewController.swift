9f8ae63a5901e1e1d3506b0820de7c77c5a9bb34
chenkun24 <chenkun0824@gmail.com>
2015-09-08 21:04:00 +0800

    commit

---
+++ CollectionViewAnimationDemo/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  CollectionViewAnimationDemo
+//
+//  Created by C.K on 9/8/15.
+//  Copyright © 2015 chenkun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var bottomView: UIView!
+    @IBOutlet var bottomHeightConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.bottomHeightConstraint.constant = 0;
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController : UIScrollViewDelegate{
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        print(scrollView.contentOffset.x)
+        if scrollView.contentOffset.x < scrollView.frame.size.width{
+            self.bottomHeightConstraint.constant = scrollView.contentOffset.x / scrollView.frame.size.width * 60;
+        }else{
+            self.bottomHeightConstraint.constant = 60;
+        }
+        self.view.setNeedsUpdateConstraints()
+        self.view.updateConstraintsIfNeeded()
+        self.view.layoutIfNeeded()
+    }
+}
+
+extension ViewController : UICollectionViewDataSource{
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MainCell", forIndexPath: indexPath)
+        
+        if indexPath.row % 2 == 0{
+            cell.backgroundColor = UIColor.redColor()
+        }else{
+            cell.backgroundColor = UIColor.greenColor()
+        }
+        
+        
+        return cell
+    }
+    
+}
+
+extension ViewController : UICollectionViewDelegate{
+    
+}
+
+extension ViewController : UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+}
